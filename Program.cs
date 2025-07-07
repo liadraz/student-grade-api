f1bb96b7dd049b8261e78b9283a9a5dc508a2e07
liad.raz <raz.liad@gmail.com>
2025-07-07 20:46:45 +0300

    Add comments

--- Program.cs
+++ Program.cs
@@ -3,22 +3,32 @@ using student_grades_api.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//
+// Builder Setup
+//
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Registers controller services (dependency injection, routing, etc.)
+builder.Services.AddControllers();
+
+// Register the DbContext with the dependency injection container
 var connString = builder.Configuration.GetConnectionString("studentGrades");
 builder.Services.AddDbContext<StudentGradesContext>(options =>
     options.UseNpgsql(connString));
 
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Middleware pipeline - register Swagger and its UI middleware that intercept requests to /swagger
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+// Route incoming HTTP requests to controllers using their [Route] attributes
+app.MapControllers();
+
 
 app.Run();
